3979243e7b76655c2c970b248f8dd8f6a81c571e
“cody-ng“ <cody-ng@users.noreply.github.com>
2015-10-19 14:39:17 -0400

    AND101-Exercise-3
    Write C# code to access UI views and perform some work on user-entered
    data

--- AND101-intro-to-android/TipCalculator/MainActivity.cs
+++ AND101-intro-to-android/TipCalculator/MainActivity.cs
@@ -12,13 +12,36 @@ namespace TipCalculator
 	[Activity (Label = "TipCalculator", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
+		EditText _inputBill;
+		Button _calculateButton;
+		TextView _outputTip;
+		TextView _outputTotal;
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-
 			SetContentView (Resource.Layout.Main);
 
+
+			_inputBill = FindViewById<EditText> (Resource.Id.inputBill);
+			_calculateButton = FindViewById<Button> (Resource.Id.calculateButton);
+			_outputTip = FindViewById<TextView> (Resource.Id.outputTip);
+			_outputTotal = FindViewById<TextView> (Resource.Id.outputTotal);
+
+			_calculateButton.Click += OnClick;
+		}
+
+		void OnClick(object sender, EventArgs e)
+		{
+			var s = _inputBill.Text;
+
+			var bill = double.Parse (s);
+
+			var tip = bill * 0.15;
+			var total = bill + tip;
+
+			_outputTotal.Text = total.ToString ();
+			_outputTip.Text = tip.ToString ();
 		}
 	}
 }
