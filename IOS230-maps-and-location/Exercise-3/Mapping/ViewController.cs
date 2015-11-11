4028652e115c404653fd75d0f3000206dd0e7df1
“cody-ng” <cody-ng@users.noreply.github.com>
2015-11-11 16:18:52 -0500

    IOS230-Exercise-3-Completed
    Work with MKMapCamera and change the camera's view for different areas
    on the map by tapping different buttons.

--- IOS230-maps-and-location/Exercise-3/Mapping/ViewController.cs
+++ IOS230-maps-and-location/Exercise-3/Mapping/ViewController.cs
@@ -10,6 +10,11 @@ namespace Mapping
 	{
         CLLocationManager locMan = new CLLocationManager();
 
+        CLLocationCoordinate2D locSanFran = new CLLocationCoordinate2D(37.7833, -122.4167);
+        CLLocationCoordinate2D locBoston = new CLLocationCoordinate2D(42.3601, -71.0589);
+        CLLocationCoordinate2D locLondon = new CLLocationCoordinate2D(51.5072, -0.1275);
+        CLLocationCoordinate2D locSingapore = new CLLocationCoordinate2D(1.3, 103.8);
+
 		public ViewController (IntPtr handle) : base (handle)
 		{
 		}
@@ -63,22 +68,42 @@ namespace Mapping
 
 		partial void btnSanFran_Activated (UIBarButtonItem sender)
 		{
-
+            map.Camera.CenterCoordinate = locSanFran;
+            map.Camera.Altitude = 5000;
 		}
 
 		partial void btnBoston_Activated (UIBarButtonItem sender)
 		{
-			
+            map.MapType = MKMapType.Standard;
+
+            map.Camera.CenterCoordinate = locBoston;
+            map.Camera.Altitude = 500;
+            map.Camera.Pitch = 45;
+
+            map.ShowsBuildings = true;
+            map.PitchEnabled = true;
 		}
 
 		partial void btnLondon_Activated (UIBarButtonItem sender)
 		{
-
+            map.Camera = new MKMapCamera {
+                CenterCoordinate = locLondon,
+                Altitude = 10000,
+                Pitch = 45,
+                Heading = 180,
+            };
 		}
 
 		partial void btnSingapore_Activated (UIBarButtonItem sender)
 		{
+            var camera = new MKMapCamera {
+                CenterCoordinate = locSingapore,
+                Altitude = 10000,
+                Pitch = 45,
+                Heading = 180
+            };
 
+            map.SetCamera(camera, true);
 
 		}
 	}
