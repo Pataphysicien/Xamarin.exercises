3c4d1e85e6f5f35110947fbf61d6cac49cac4b06
“cody-ng” <cody-ng@users.noreply.github.com>
2015-11-11 15:13:46 -0500

    IOS230-Exercise-2-Completed
    Request "in-use" location permissions from iOS and display the device's
    current location on your map.

--- IOS230-maps-and-location/Exercise-1-And-2/Mapping/MappingViewController.cs
+++ IOS230-maps-and-location/Exercise-1-And-2/Mapping/MappingViewController.cs
@@ -3,11 +3,14 @@ using System.Drawing;
 
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using MonoTouch.CoreLocation;
 
 namespace Mapping
 {
     public partial class MappingViewController : UIViewController
     {
+        CLLocationManager locMan = new CLLocationManager();
+
         public MappingViewController (IntPtr handle) : base (handle)
         {
         }
@@ -17,6 +20,47 @@ namespace Mapping
             base.ViewDidLoad ();
             
             // Perform any additional setup after loading the view, typically from a nib.
+            if (UIDevice.CurrentDevice.CheckSystemVersion (8, 0) == true)
+            {
+                locMan.RequestWhenInUseAuthorization ();
+            }
+        }
+        public override void ViewDidAppear (bool animated)
+        {
+            if (CLLocationManager.Status == CLAuthorizationStatus.Denied)
+            {
+                if (UIDevice.CurrentDevice.CheckSystemVersion (8, 0) == true)
+                {
+                    var yesNoAlertController = UIAlertController.Create (
+                                                   "Unable to determien location",
+                                                   "This application works best when it can determine yoru current " +
+                                                   "position.  Would you like to go to Settings to enable location data?",
+                                                   UIAlertControllerStyle.Alert);
+
+                    yesNoAlertController.AddAction (UIAlertAction.Create (
+                        "Yes", UIAlertActionStyle.Default,
+                        alert =>
+                        {
+                            var settingString = UIApplication.OpenSettingsUrlString;
+                            var url = new NSUrl (settingString);
+                            UIApplication.SharedApplication.OpenUrl (url);
+                        }));
+
+                    yesNoAlertController.AddAction (UIAlertAction.Create (
+                        "No", UIAlertActionStyle.Cancel, null));
+
+                    this.PresentViewController (yesNoAlertController, true, null);
+                }
+                else
+                {
+                    var alert = new UIAlertView ("Unabled to determine location",
+                        "This application works best when it can determine your current " +
+                        "position.  Please open the Settings and enable Location Services " +
+                        "for this app.", 
+                        null,"OK");
+                    alert.Show ();
+                }
+            }
         }
 
         public override void DidReceiveMemoryWarning ()
