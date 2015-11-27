50c0a31983ce2aad72c54059286577d6de1b1535
cody-ng <cody-ng@users.noreply.github.com>
2015-11-27 17:24:02 -0500

    AND205-Exercise-2
    Tab Navigation

---
+++ AND205-navigation-patterns/Exercise-2/Clock/ClockAdapter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Java.Lang;
+
+namespace Clock
+{
+	public class ClockAdapter : Android.Support.V4.App.FragmentPagerAdapter
+	{
+        Android.Support.V4.App.Fragment[] _fragments;
+        ICharSequence[] _titles;
+
+
+		public ClockAdapter(Android.Support.V4.App.FragmentManager fm, Android.Support.V4.App.Fragment[] fragments, ICharSequence[] titles)
+			: base(fm)
+		{
+            _fragments = fragments;
+            _titles = titles;
+		}
+
+		public override int Count
+		{
+			get
+			{
+                return _fragments.Length;
+			}
+		}
+
+		public override Android.Support.V4.App.Fragment GetItem(int position)
+		{
+            return _fragments [position];
+		}
+
+		public override ICharSequence GetPageTitleFormatted(int position)
+		{
+            return _titles [position];
+		}
+	}
+}
---
+++ AND205-navigation-patterns/Exercise-2/Clock/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Clock
+{
+	[Activity(Label = "Clock", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Android.Support.V4.App.FragmentActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			SetContentView(Resource.Layout.Main);
+
+			var fragments = new Android.Support.V4.App.Fragment[]
+				{
+					new TimeFragment     (),
+					new StopwatchFragment(),
+					new AboutFragment    ()
+				};
+
+			var titles = CharSequence.ArrayFromStringArray(new [] 
+				{ 
+					"Time", 
+					"Stopwatch", 
+					"About"
+				});
+			
+			//
+			// TODO: Use FindViewById to get a reference to the ViewPager in the UI
+			//
+            var viewpage = FindViewById<Android.Support.V4.View.ViewPager> (Resource.Id.viewPager);
+
+
+			//
+			// TODO: Create a ClockAdapter
+			//
+            var adapter = new ClockAdapter (base.SupportFragmentManager, fragments, titles);
+
+
+
+			//
+			// TODO: Assign the ClockAdapter to the ViewPager's Adapter property
+			//
+            viewpage.Adapter = adapter;
+
+
+
+		}
+	}
+}
